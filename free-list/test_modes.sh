649d4dbca730012c48817310a8b3cbc6cd6118d1
Thassilo Schulze <thassilo.schulze@proton.me>
2024-03-03 09:23:06 +0100

    Rename free_list1.c to free_list.c

--- free-list/test_modes.sh
+++ free-list/test_modes.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
-# Test all three modes in free_list1.c
+# Test all three modes in free_list.c
 
 modes=("FIRST" "NEXT" "BEST")
 
 for mode in "${modes[@]}"; do
-    gcc -DSEARCH_MODE="${mode}"_FIT -g free_list1.c && ./a.out
+    gcc -DSEARCH_MODE="${mode}"_FIT -g free_list.c && ./a.out
 done
 
 rm a.out
