fb794142764cb068c563179b0d8a754fa424e4ce
Thassilo Schulze <thassilo.schulze@proton.me>
2024-03-10 16:15:35 +0100

    Add block re-use to the segregated free list allocator

--- free-list/segregated_free_list.c
+++ free-list/segregated_free_list.c
@@ -32,11 +32,11 @@ enum {
 };
 
 static BlockHdr *global_buckets[] = {
-    [TINY_IDX] = NULL,  /* >= TINY words */
-    [SMALL_IDX] = NULL, /* >= SMALL words */
-    [MID_IDX] = NULL,   /* >= MID words */
-    [BIG_IDX] = NULL,   /* >= BIG words */
-    [HUGE_IDX] = NULL,  /* >= HUGE words */
+    [TINY_IDX] = NULL,  /* >= TINY # of words */
+    [SMALL_IDX] = NULL, /* >= SMALL # of words */
+    [MID_IDX] = NULL,   /* >= MID # of words */
+    [BIG_IDX] = NULL,   /* >= BIG # of words */
+    [HUGE_IDX] = NULL,  /* >= HUGE # of words */
 };
 
 static void *heap_base_addr = NULL;
@@ -55,10 +55,46 @@ void reset_heap(void) {
 
 BlockHdr *hdr(word_t *ptr) { return (BlockHdr *)(ptr)-1; }
 
-BlockHdr *find_block(size_t size) { return NULL; }
+/*
+ * Return the index of the bucket that stores blocks
+ * that have a size greater or equal to SIZE.
+ */
+int bucket_idx(size_t size) {
+  int idx = 0;
+  size_t words = size / sizeof(word_t);
+  if (words >= HUGE) {
+    idx = HUGE_IDX;
+  } else if (words >= BIG) {
+    idx = BIG_IDX;
+  } else if (words >= MID) {
+    idx = MID_IDX;
+  } else if (words >= SMALL) {
+    idx = SMALL_IDX;
+  } else if (words >= TINY) {
+    idx = TINY_IDX;
+  }
+  return idx;
+}
+
+BlockHdr *find_block(size_t size) {
+  int idx = bucket_idx(size);
+  BlockHdr *blk = global_buckets[idx];
+  BlockHdr *best = NULL;
+
+  while (blk != NULL) {
+    if (blk->used == FALSE && blk->size >= size) {
+      if (best == NULL || blk->size < best->size) {
+        best = blk;
+      }
+    }
+    blk = blk->next;
+  }
+
+  return best;
+}
 
 BlockHdr *request_block_from_os(size_t size) {
-  BlockHdr *blk = sbrk(0); /* Return current break. */
+  BlockHdr *blk = sbrk(0); /* Returns current break. */
 
   /*
    * Safe the start address of the heap before changing
@@ -75,26 +111,6 @@ BlockHdr *request_block_from_os(size_t size) {
   }
 
   return blk;
-}
-
-/* Insert a block into the free list that best fits its size. */
-void insert_blk(BlockHdr *blk) {
-  int idx = 0;
-  size_t words = blk->size / sizeof(word_t);
-  if (words >= HUGE) {
-    idx = HUGE_IDX;
-  } else if (words >= BIG) {
-    idx = BIG_IDX;
-  } else if (words >= MID) {
-    idx = MID_IDX;
-  } else if (words >= SMALL) {
-    idx = SMALL_IDX;
-  } else if (words >= TINY) {
-    idx = TINY_IDX;
-  }
-
-  blk->next = global_buckets[idx];
-  global_buckets[idx] = blk;
 }
 
 /* Align the given size to word boundaries. */
@@ -115,12 +131,18 @@ word_t *alloc(ptrdiff_t ssize) {
 
   BlockHdr *blk = NULL;
   if ((blk = find_block(size)) != NULL) {
+    blk->used = TRUE;
     return (word_t *)(blk + 1);
   } else {
     blk = request_block_from_os(size);
     blk->size = size;
     blk->used = TRUE;
-    insert_blk(blk);
+
+    /* Insert the block into the right bucket. */
+    int idx = bucket_idx(blk->size);
+    blk->next = global_buckets[idx];
+    global_buckets[idx] = blk;
+
     return (word_t *)(blk + 1);
   }
 }
@@ -189,6 +211,66 @@ int main(void) {
     assert(hdr(a2)->used == FALSE);
     assert(hdr(a2)->size == 256);
     assert(global_buckets[MID_IDX] == hdr(a2));
+  }
+
+  {
+    reset_heap();
+    dbg("TEST: Re-using block\n");
+    /* Re-use the same block for a1 and a2. */
+    word_t *a1 = alloc(64);
+    wfree(a1);
+    assert(hdr(a1)->used == FALSE);
+    assert(global_buckets[TINY_IDX] == hdr(a1));
+    word_t *a2 = alloc(64);
+    assert(hdr(a1) == hdr(a2));
+    assert(hdr(a1)->used == TRUE);
+    wfree(a2);
+
+    /* Allocate a new block for an allocation larger than a1. */
+    word_t *a3 = alloc(65);
+    assert(hdr(a3) != hdr(a1));
+    assert(hdr(a3)->size == 72);
+    assert(hdr(a3)->used == TRUE);
+    assert(global_buckets[TINY_IDX] == hdr(a3));
+    assert(global_buckets[TINY_IDX]->next == hdr(a1));
+    wfree(a3);
+
+    /* Re-use the smaller of the two free blocks in the TINY bucket. */
+    word_t *a4 = alloc(64);
+    assert(hdr(a4) == hdr(a1));
+    assert(hdr(a4)->used == TRUE);
+    assert(hdr(a4)->size == 64);
+    assert(global_buckets[TINY_IDX] == hdr(a3));
+    assert(global_buckets[TINY_IDX]->next == hdr(a4));
+
+    /*
+     * Create a free block in the SMALL bucket. For the subsequent SMALL
+     * allocations, that block should be re-used.
+     */
+    word_t *a5 = alloc(128);
+    assert(global_buckets[SMALL_IDX] == hdr(a5));
+    assert(hdr(a5)->used == TRUE);
+    assert(hdr(a5)->size == 128);
+    wfree(a5);
+    word_t *a6 = alloc(128);
+    assert(hdr(a6) == hdr(a5));
+    assert(hdr(a6)->used == TRUE);
+    assert(hdr(a6)->size == 128);
+    assert(global_buckets[SMALL_IDX] == hdr(a6));
+    wfree(a6);
+
+    /*
+     * Now there are free blocks in two buckets, TINY and SMALL.
+     * If we allocate another block, the smallest possible bucket
+     * should be picked.
+     */
+    assert(global_buckets[TINY_IDX]->used == FALSE);
+    word_t *a7 = alloc(65);
+    assert(hdr(a7) == hdr(a3));
+    assert(hdr(a7)->used == TRUE);
+    assert(hdr(a7)->size == 72);
+    assert(global_buckets[TINY_IDX] == hdr(a7));
+    wfree(a7);
   }
 
   return 0;
